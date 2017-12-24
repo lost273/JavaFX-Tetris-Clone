23a68954608d0fc532204cd0845959e7c01a9138
lost273 <dnk273@gmail.com>
2017-12-24 18:45:38 +0300

    24-12-2017

---
+++ src/main/java/com/quirko/logic/bricks/GBrick.java
@@ -0,0 +1,28 @@
+
+package com.quirko.logic.bricks;
+import com.quirko.logic.MatrixOperations;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GBrick implements Brick{
+    private final List<int[][]> brickMatrix = new ArrayList<>();
+
+    public GBrick() {
+        brickMatrix.add(new int[][]{
+                {0, 0, 0, 0},
+                {7, 7, 0, 0},
+                {0, 7, 7, 0},
+                {0, 0, 0, 0}
+        });
+        brickMatrix.add(new int[][]{
+                {0, 7, 0, 0},
+                {7, 7, 0, 0},
+                {7, 0, 0, 0},
+                {0, 0, 0, 0}
+        });
+    
+    @Override
+    public List<int[][]> getShapeMatrix() {
+        return MatrixOperations.deepCopyList(brickMatrix);
+    }
+}
